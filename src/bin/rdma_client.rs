8b4b911781bf2fcdcbdbb3d1727a815a2152c021
Neve Noomo <d3ad.l1n3@yandex.ru>
2020-02-24 18:41:10 +0300

    Update client

--- src/bin/rdma_client.rs
+++ src/bin/rdma_client.rs
@@ -1,6 +1,6 @@
 use netcat::connection::rdma::RdmaServerConnector;
 
 fn main() {
-    let _client = RdmaServerConnector::new("127.0.0.1:9003");
+    let _client = RdmaServerConnector::new("10.0.2.4:9003");
     println!("RDMA handshake successful!");
 }
