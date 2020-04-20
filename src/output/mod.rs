710a389e617858ae52b9d5cf120ef049f4f768c6
Neve Noomo <d3ad.l1n3@yandex.ru>
2020-04-20 12:37:23 +0300

    Fix error

--- src/output/mod.rs
+++ src/output/mod.rs
@@ -18,8 +18,34 @@ pub mod file {
     use serde_json::to_writer;
     use std::io::{Error, ErrorKind, Result, Write};
 
+    /// Outputs data in JSON format to the underlying writer
+    pub struct JsonRecorder<W: Write>(W);
+
+    impl<W: Write> JsonRecorder<W> {
+        /// Wraps provided value in `JsonRecorder`
+        pub fn new(w: W) -> JsonRecorder<W> {
+            JsonRecorder(w)
+        }
+
+        /// Unwraps this `JsonRecorder<W>`, returns the underlying writer.
+        pub fn into_inner(self) -> Result<W> {
+            Ok(self.0)
+        }
+    }
+
+    impl<W: Write> Write for JsonRecorder<W> {
+        // IDEA maybe add json validation
+        fn write(&mut self, buf: &[u8]) -> Result<usize> {
+            self.0.write(buf) 
+        }
+
+        fn flush(&mut self) -> Result<()> {
+            self.0.flush()
+        }
+    }
+
     /// Writes json-serialized data
-    impl<T: Serialize, W: Write> Record<T> for W {
+    impl<T: Serialize, W: Write> Record<T> for JsonRecorder<W> {
         fn record(&mut self, data: T) -> Result<()> {
             to_writer(self, &data).map_err(|e| Error::new(ErrorKind::InvalidData, e))
         }
@@ -28,6 +54,7 @@ pub mod file {
 
 pub mod vec {
     use super::Record;
+    use std::io::Result;
 
     /// Just pushes the data to a vector
     impl<T> Record<T> for Vec<T> {
